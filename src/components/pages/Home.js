502a1c83f5706d8515c943cc16b5fdc61cd7027c
MaciejUlasiuk <filip123xd@o2.pl>
2021-07-08 21:02:42 +0200

    big update

---
+++ src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
+
+function Home() {
+  return (
+    <>
+    {/* ZROB TO DO KONCA 7 LIPCA I NIE ZAPOMNIJ */}
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
