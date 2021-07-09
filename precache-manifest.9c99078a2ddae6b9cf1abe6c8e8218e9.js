9e18dbf6e116238d37d25d86a128bb1adaa19c87
MaciejUlasiuk <filip123xd@o2.pl>
2021-07-09 14:31:06 +0200

    Updates

---
+++ precache-manifest.9c99078a2ddae6b9cf1abe6c8e8218e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23d55d0f9654c99266af0b28fb46715",
+    "url": "/responsive-react-website/index.html"
+  },
+  {
+    "revision": "c22acc196427ee8e7e77",
+    "url": "/responsive-react-website/static/css/main.e94b55a0.chunk.css"
+  },
+  {
+    "revision": "986535b1701544563922",
+    "url": "/responsive-react-website/static/js/2.5cd5786a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/responsive-react-website/static/js/2.5cd5786a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22acc196427ee8e7e77",
+    "url": "/responsive-react-website/static/js/main.abc5a97b.chunk.js"
+  },
+  {
+    "revision": "f813e02158134e55db5a",
+    "url": "/responsive-react-website/static/js/runtime-main.1595831f.js"
+  }
+]);
